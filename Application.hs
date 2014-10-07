fcff926e52f07b9e531919becbdf33b5e5f257f6
Sajith Sasidharan <sajith@nonzen.in>
2014-10-07 16:04:07 -0400

    Added /profile/start handler (Ref: issue #13)

--- Application.hs
+++ Application.hs
@@ -34,6 +34,7 @@ import Handler.Privacy
 import Handler.Terms
 import Handler.Security
 import Handler.Profile
+import Handler.ProfileStart
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/ProfileStart.hs
@@ -0,0 +1,9 @@
+module Handler.ProfileStart where
+
+import Import
+
+getProfileStartR :: Handler Html
+getProfileStartR = error "Not yet implemented: getProfileStartR"
+
+postProfileStartR :: Handler Html
+postProfileStartR = error "Not yet implemented: postProfileStartR"
