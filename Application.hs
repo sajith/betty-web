868b2a70d0e10c1ea2a489c7bf67b2d14c4d8af0
Sajith Sasidharan <sajith@nonzen.in>
2014-10-07 16:08:40 -0400

    Added /history/bg handler (Ref: issue #14)

--- Application.hs
+++ Application.hs
@@ -36,6 +36,7 @@ import Handler.Security
 import Handler.Profile
 import Handler.ProfileStart
 import Handler.AddBG
+import Handler.HistoryBG
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/HistoryBG.hs
@@ -0,0 +1,6 @@
+module Handler.HistoryBG where
+
+import Import
+
+getHistoryBGR :: Handler Html
+getHistoryBGR = error "Not yet implemented: getHistoryBGR"
