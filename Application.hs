b347c7ed558090e18eee037bdcce0ee53b433556
Sajith Sasidharan <sajith@nonzen.in>
2014-10-07 15:50:51 -0400

    Adding /info/terms handler (#10)

--- Application.hs
+++ Application.hs
@@ -31,6 +31,7 @@ import Handler.About
 import Handler.ContactR
 import Handler.FaqR
 import Handler.Privacy
+import Handler.Terms
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/Terms.hs
@@ -0,0 +1,6 @@
+module Handler.Terms where
+
+import Import
+
+getTermsR :: Handler Html
+getTermsR = error "Not yet implemented: getTermsR"
