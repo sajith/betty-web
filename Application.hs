41e0135faa1712002f7a9590cfdf0d74794edead
Sajith Sasidharan <sajith@nonzen.in>
2014-10-07 15:51:35 -0400

    Adding /info/security handler (Ref: issue #11)

--- Application.hs
+++ Application.hs
@@ -32,6 +32,7 @@ import Handler.ContactR
 import Handler.FaqR
 import Handler.Privacy
 import Handler.Terms
+import Handler.Security
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/Security.hs
@@ -0,0 +1,6 @@
+module Handler.Security where
+
+import Import
+
+getSecurityR :: Handler Html
+getSecurityR = error "Not yet implemented: getSecurityR"
