828a867539e98e1d70c3cce1c025d2da6c47db59
Sajith Sasidharan <sajith@nonzen.in>
2014-10-07 16:07:12 -0400

    Added /add/bg handler (Ref: issue #14)

--- Application.hs
+++ Application.hs
@@ -35,6 +35,7 @@ import Handler.Terms
 import Handler.Security
 import Handler.Profile
 import Handler.ProfileStart
+import Handler.AddBG
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/AddBG.hs
@@ -0,0 +1,9 @@
+module Handler.AddBG where
+
+import Import
+
+getAddBGR :: Handler Html
+getAddBGR = error "Not yet implemented: getAddBGR"
+
+postAddBGR :: Handler Html
+postAddBGR = error "Not yet implemented: postAddBGR"
