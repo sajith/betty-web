ca809fe07cacd8658b55acca8b9e9508ca4359f5
Sajith Sasidharan <sajith@nonzen.in>
2014-10-07 15:42:31 -0400

    Adding /info/privacy handler (Ref: issue #9)

--- Application.hs
+++ Application.hs
@@ -30,6 +30,7 @@ import Handler.Home
 import Handler.About
 import Handler.ContactR
 import Handler.FaqR
+import Handler.Privacy
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/Privacy.hs
@@ -0,0 +1,6 @@
+module Handler.Privacy where
+
+import Import
+
+getPrivacyR :: Handler Html
+getPrivacyR = error "Not yet implemented: getPrivacyR"
