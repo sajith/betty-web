168accab328077be2498aae35542b6fb7256a0f1
Sajith Sasidharan <sajith@nonzen.in>
2014-10-10 14:45:38 -0400

    Added a skeletal AddBGR

--- Handler/AddBG.hs
+++ Handler/AddBG.hs
@@ -1,9 +1,81 @@
 module Handler.AddBG where
 
 import Import
+import Yesod.Auth          (requireAuth)
+
+import Data.Text           (pack)
+import Data.Time           (Day, TimeOfDay)
+import Data.Time.Clock     (getCurrentTime)
+import Data.Time.LocalTime (minutesToTimeZone, timeZoneOffsetString)
+
+import Betty.Model
+
+------------------------------------------------------------------------
+
+-- In this handler, I'm choosing an input form over applicative or
+-- monadic forms for the moment (although applicative or monadic form
+-- would be elegant and better functioning), since that seems to give
+-- better control over the UI.
+
+------------------------------------------------------------------------
+
+data BGData =
+    BGData {
+        date    :: Day
+        , time  :: TimeOfDay
+        , tz    :: Maybe Int
+        , value :: Double
+        , notes :: Maybe Text
+        } deriving (Show)
+
+------------------------------------------------------------------------
 
 getAddBGR :: Handler Html
-getAddBGR = error "Not yet implemented: getAddBGR"
+getAddBGR = do
+    _ <- requireAuth
+
+    defaultLayout $ do
+        setTitle "Project D : Add Blood Sugar"
+        $(widgetFile "bg.entry")
+
+------------------------------------------------------------------------
 
 postAddBGR :: Handler Html
-postAddBGR = error "Not yet implemented: postAddBGR"
+postAddBGR = do
+
+    bgdata <- runInputPost $ BGData
+             <$> ireq dayField "date"
+             <*> ireq timeField "time"
+             <*> iopt intField "timezone"
+             <*> ireq doubleField "value"
+             <*> iopt textField "notes"
+
+    Entity uid u <- requireAuth
+
+    utctime <- liftIO getCurrentTime
+
+    let tzs = fmap (pack . timeZoneOffsetString . minutesToTimeZone) $ tz bgdata
+
+    -- TODO: get preferred unit from profile.
+    let unit = MgDL
+
+    let record = BloodGlucoseHistory
+                 uid
+                 utctime         -- utc time
+                 -- Nothing      -- local zonedtime
+                                 -- TODO: add this field
+                 (date bgdata)   -- date as set by client
+                 (time bgdata)   -- time as set by client
+                 tzs             -- tz as set by client
+                 (value bgdata)  -- blood sugar value
+                 (Just unit)     -- blood sugar unit
+                 (notes bgdata)  -- notes, if any
+
+    _ <- runDB $ insert record
+
+    -- TODO: log more stuff.
+    $(logDebug) "[POST] Recorded blood glucose"
+
+    -- TODO: avoid the redirect.
+    redirect $ HistoryBGR
+
