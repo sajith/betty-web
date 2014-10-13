3c0af68a03a176fa6fac9acf6c05bf76645b379a
Sajith Sasidharan <sajith@nonzen.in>
2014-10-13 17:59:46 -0400

    Adding ProfileR GET handler.

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -1,9 +1,26 @@
 module Handler.Profile where
 
 import Import
+import Yesod.Auth (requireAuth)
+
+------------------------------------------------------------------------
 
 getProfileR :: Handler Html
-getProfileR = error "Not yet implemented: getProfileR"
+getProfileR = do
+    Entity _ u <- requireAuth
+
+    defaultLayout $ do
+        setTitle "Project D: Your Profile"
+        $(widgetFile "profile")
+        $(widgetFile "profile.password")
+        $(widgetFile "profile.email")
+        $(widgetFile "profile.timezone")
+        $(widgetFile "profile.units")
+        $(widgetFile "profile.delete")
+
+------------------------------------------------------------------------
 
 postProfileR :: Handler Html
 postProfileR = error "Not yet implemented: postProfileR"
+
+------------------------------------------------------------------------
