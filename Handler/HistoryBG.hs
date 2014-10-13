1684c7fdec504ddde64f065d18cd9fed8f08aa93
Sajith Sasidharan <sajith@nonzen.in>
2014-10-13 15:30:59 -0400

    Adding a GET handler in HistoryBGR.

--- Handler/HistoryBG.hs
+++ Handler/HistoryBG.hs
@@ -1,6 +1,20 @@
 module Handler.HistoryBG where
 
 import Import
+import Yesod.Auth (requireAuth)
+
+------------------------------------------------------------------------
 
 getHistoryBGR :: Handler Html
-getHistoryBGR = error "Not yet implemented: getHistoryBGR"
+getHistoryBGR = do
+    Entity uid u <- requireAuth
+
+    -- TODO: pagination
+    sugars <- fmap (map entityVal) $
+              runDB $ selectList [BloodGlucoseHistoryUid ==. uid] [LimitTo 10]
+
+    defaultLayout $ do
+        setTitle "Project D : Blood Sugar Logs"
+        $(widgetFile "bg.history")
+
+------------------------------------------------------------------------
