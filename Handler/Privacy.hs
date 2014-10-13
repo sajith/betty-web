38bdfac9d0c40120a90f1fa0e5de6bfd25daa339
Sajith Sasidharan <sajith@nonzen.in>
2014-10-13 16:13:45 -0400

    Adding some blurb in the privacy policy page.

--- Handler/Privacy.hs
+++ Handler/Privacy.hs
@@ -2,5 +2,11 @@ module Handler.Privacy where
 
 import Import
 
+------------------------------------------------------------------------
+
 getPrivacyR :: Handler Html
-getPrivacyR = error "Not yet implemented: getPrivacyR"
+getPrivacyR = defaultLayout $ do
+    setTitle "Project D : Privacy Policy"
+    $(widgetFile "info.privacy")
+
+------------------------------------------------------------------------
