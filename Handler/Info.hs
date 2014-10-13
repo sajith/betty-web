f25839b1cb5b5a9378134d2cdd525f359ce16e54
Sajith Sasidharan <sajith@nonzen.in>
2014-10-13 16:36:34 -0400

    Rearranging furniture: exposing /info/* handlers via Handler.Info module.

---
+++ Handler/Info.hs
@@ -0,0 +1,14 @@
+module Handler.Info
+       (  getAboutR
+       , getContactR
+       , getPrivacyR
+       , getSecurityR
+       , getTermsR
+       ) where
+
+import Handler.Info.About
+import Handler.Info.Contact
+import Handler.Info.Privacy
+import Handler.Info.Security
+import Handler.Info.Terms
+
