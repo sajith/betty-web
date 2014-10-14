e2ec53a48ffac042320c1299f19125754b6e43d9
Sajith Sasidharan <sajith@nonzen.in>
2014-10-14 11:48:44 -0400

    Being (slightly more) consistent in naming things.

--- Betty/Vendor.hs
+++ Betty/Vendor.hs
@@ -23,13 +23,13 @@ timepickerThemeCss = vendor_jquery_ui_timepicker_0_3_3_include_ui_1_10_0_ui_ligh
 
 ------------------------------------------------------------------------
 
-jqPlotCSS :: StaticRoute
-jqPlotCSS = vendor_jqplot_1_0_8_jquery_jqplot_min_css
+jqPlotCss :: StaticRoute
+jqPlotCss = vendor_jqplot_1_0_8_jquery_jqplot_min_css
 
-jqPlotJS :: StaticRoute
-jqPlotJS = vendor_jqplot_1_0_8_jquery_jqplot_min_js
+jqPlotJs :: StaticRoute
+jqPlotJs = vendor_jqplot_1_0_8_jquery_jqplot_min_js
 
-jqPlotDateAxisJS :: StaticRoute
-jqPlotDateAxisJS = vendor_jqplot_1_0_8_plugins_jqplot_dateAxisRenderer_min_js
+jqPlotDateAxisJs :: StaticRoute
+jqPlotDateAxisJs = vendor_jqplot_1_0_8_plugins_jqplot_dateAxisRenderer_min_js
 
 ------------------------------------------------------------------------
