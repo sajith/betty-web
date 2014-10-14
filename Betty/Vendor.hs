38714a1c2d76844efbea4c0a6f1c289ae27d0bbd
Sajith Sasidharan <sajith@nonzen.in>
2014-10-14 11:57:27 -0400

    Adding jQuery UI JS/CSS and jQuery UI timepicker JS paths in Betty.Vendor.

--- Betty/Vendor.hs
+++ Betty/Vendor.hs
@@ -15,8 +15,19 @@ faCss = vendor_font_awesome_4_2_0_css_font_awesome_min_css
 
 ------------------------------------------------------------------------
 
+jQueryUiCss :: StaticRoute
+jQueryUiCss = vendor_jquery_ui_1_11_1_jquery_ui_min_css
+
+jQueryUiJs :: StaticRoute
+jQueryUiJs = vendor_jquery_ui_1_11_1_jquery_ui_min_js
+
+------------------------------------------------------------------------
+
 timepickerCss :: StaticRoute
 timepickerCss = vendor_jquery_ui_timepicker_0_3_3_jquery_ui_timepicker_css
+
+timepickerJs :: StaticRoute
+timepickerJs = vendor_jquery_ui_timepicker_0_3_3_jquery_ui_timepicker_js
 
 timepickerThemeCss :: StaticRoute
 timepickerThemeCss = vendor_jquery_ui_timepicker_0_3_3_include_ui_1_10_0_ui_lightness_jquery_ui_1_10_0_custom_min_css
