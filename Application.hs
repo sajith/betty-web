0d1b1f7eac6964fd46f7dbc24e3f942f0409ebaa
Sajith Sasidharan <sajith@nonzen.in>
2015-04-17 13:31:36 -0400

    Add empty /api/v0/sugar/add handler.

--- Application.hs
+++ Application.hs
@@ -38,6 +38,7 @@ import           Handler.Home
 import           Handler.Info
 import           Handler.Profile
 import           Handler.ProfileStart
+import           Handler.ApiV0SugarAdd
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ Handler/ApiV0SugarAdd.hs
@@ -0,0 +1,9 @@
+module Handler.ApiV0SugarAdd where
+
+import Import
+
+getApiV0SugarAddR :: Handler Html
+getApiV0SugarAddR = error "Not yet implemented: getApiV0SugarAddR"
+
+postApiV0SugarAddR :: Handler Html
+postApiV0SugarAddR = error "Not yet implemented: postApiV0SugarAddR"
