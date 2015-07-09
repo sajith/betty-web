4534d0e984ccb91ce968d9f25be18925e9949341
Sajith Sasidharan <sajith@nonzen.in>
2015-07-09 12:35:44 -0400

    minor.

--- Application.hs
+++ Application.hs
@@ -33,12 +33,12 @@ import           Betty.Pid                            (writePidFile)
 -- Import all relevant handler modules here.
 -- Don't forget to add new modules to your cabal file!
 import           Handler.AddBG
+import           Handler.Api
 import           Handler.HistoryBG
 import           Handler.Home
 import           Handler.Info
 import           Handler.Profile
 import           Handler.ProfileStart
-import           Handler.Api
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
