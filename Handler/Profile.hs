c47535fa1522355ae2610d8b366301274f69353f
Sajith Sasidharan <sajith@nonzen.in>
2015-12-11 11:36:32 -0500

    Show a panel about "API key" in profile.

--- Handler/Profile.hs
+++ Handler/Profile.hs
@@ -1,13 +1,16 @@
 module Handler.Profile where
 
 import Import
-import Yesod.Auth (requireAuth)
+
+import Betty.Token (getToken')
+import Yesod.Auth  (requireAuth)
 
 ------------------------------------------------------------------------
 
 getProfileR :: Handler Html
 getProfileR = do
     Entity _ u <- requireAuth
+    token <- getToken' $ userEmail u
 
     defaultLayout $ do
         setTitle "Project D: Your Profile"
@@ -16,6 +19,7 @@ getProfileR = do
         $(widgetFile "profile.email")
         $(widgetFile "profile.timezone")
         $(widgetFile "profile.units")
+        $(widgetFile "profile.apikey")
         $(widgetFile "profile.delete")
 
 ------------------------------------------------------------------------
