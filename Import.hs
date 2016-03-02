46d0a526ca355f1fc1d166888a8fc626ad6e5a4b
Sajith Sasidharan <sajith@nonzen.in>
2016-03-02 10:22:22 -0500

    import Import.hs from new scaffold.
    See gh-13.

--- Import.hs
+++ Import.hs
@@ -2,33 +2,5 @@ module Import
     ( module Import
     ) where
 
-import           Prelude              as Import hiding (head, init, last,
-                                                 readFile, tail, writeFile)
-import           Yesod                as Import hiding (Route (..))
-
-#if __GLASGOW_HASKELL__ < 710
-import           Control.Applicative  as Import (pure, (<$>), (<*>))
-#endif
-
-import           Data.Text            as Import (Text)
-
-import           Foundation           as Import
-import           Model                as Import
-import           Settings             as Import
-import           Settings.Development as Import
-import           Settings.StaticFiles as Import
-
-#if __GLASGOW_HASKELL__ < 710
-#if __GLASGOW_HASKELL__ >= 704
-import           Data.Monoid          as Import
-                                                 (Monoid (mappend, mempty, mconcat),
-                                                 (<>))
-#else
-import           Data.Monoid          as Import
-                                                 (Monoid (mappend, mempty, mconcat))
-
-infixr 5 <>
-(<>) :: Monoid m => m -> m -> m
-(<>) = mappend
-#endif
-#endif
+import Foundation            as Import
+import Import.NoFoundation   as Import
