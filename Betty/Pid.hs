1030d76a9c558b55fc444a8055cd34ce9963f47c
Sajith Sasidharan <sajith@nonzen.in>
2016-03-10 09:05:03 -0500

    catch error from getAppUserDataDirectory.

--- Betty/Pid.hs
+++ Betty/Pid.hs
@@ -3,6 +3,7 @@ module Betty.Pid ( writePidFile ) where
 ------------------------------------------------------------------------
 
 import ClassyPrelude.Yesod
+
 import System.Directory     (createDirectoryIfMissing,
                              getAppUserDataDirectory)
 import System.IO            (IOMode (WriteMode), hPrint, withFile)
@@ -16,15 +17,29 @@ writePidFile :: IO ()
 writePidFile = do
 
     -- TODO: catch errors from System.Directory functions.
+    -- TODO: log errors.
     -- TODO: check permissions for appDir.
     -- TODO: hardcoded app name, fix.
 
-    appDir <- getAppUserDataDirectory "betty"
+    appDir <- getAppUserDataDirectory "betty" `catch` handleEx
+
     let pidFile = appDir </> "betty.pid"
 
     createDirectoryIfMissing True appDir
 
     pid <- getProcessID
     withFile pidFile WriteMode $ \h -> hPrint h pid
+
+    where handleEx e
+              | isDoesNotExistError e = do
+                    -- $(logError) "getAppUserDataDirectory failed with"
+                    --     <> "isDoesNotExistError. "
+                    --     <> "Using /home/betty/.betty as the fall back."
+                    return "/home/betty/.betty"
+              | otherwise             = do
+                    -- $(logError) "getAppUserDataDirectory failed! "
+                    --     <> " (UnsupportedOperation, perhaps)"
+                    --     <> "Using /tmp/betty as the fall back."
+                    return "/tmp/betty"
 
 ------------------------------------------------------------------------
