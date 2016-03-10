0115081ac3425386a40db249c630e6af969c0183
Sajith Sasidharan <sajith@nonzen.in>
2016-03-10 08:03:53 -0500

    add json/show instances of BGUnit and WtUnit

--- Betty/Model.hs
+++ Betty/Model.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Betty.Model where
 
 import ClassyPrelude.Yesod
@@ -6,16 +8,30 @@ import ClassyPrelude.Yesod
 
 -- Blood glucose units.
 data BGUnit = MgDL | Mmol
-            deriving (Show, Read, Eq, Enum, Bounded)
+            deriving (Read, Eq, Enum, Bounded, Generic)
 
 derivePersistField "BGUnit"
+
+instance FromJSON BGUnit
+instance ToJSON BGUnit
+
+instance Show BGUnit where
+    show MgDL = "mg/dL"
+    show Mmol = "mmol"
 
 ------------------------------------------------------------------------
 
 -- Weight units.
 data WtUnit = Kg | Lb
-     deriving (Show, Read, Eq, Enum, Bounded)
+            deriving (Read, Eq, Enum, Bounded, Generic)
 
 derivePersistField "WtUnit"
+
+instance FromJSON WtUnit
+instance ToJSON WtUnit
+
+instance Show WtUnit where
+    show Kg = "kilogram"
+    show Lb = "pound"
 
 ------------------------------------------------------------------------
