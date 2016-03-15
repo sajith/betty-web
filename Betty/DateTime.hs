fa4f3bb2995dc800c5cab37092fa621c811cd36e
Sajith Sasidharan <sajith@nonzen.in>
2016-03-15 15:57:25 -0400

    Add date/time formatters.

---
+++ Betty/DateTime.hs
@@ -0,0 +1,52 @@
+-- | Date and time formatting routines.
+
+module Betty.DateTime where
+
+import Data.Time
+import Import
+
+------------------------------------------------------------------------
+
+-- See `formatTime` documentation for format specifiers.
+
+dateFormat :: String
+dateFormat = "%e %b %Y"
+
+dateFormatTZ :: String
+dateFormatTZ = "%e %b %Y %Z"
+
+timeFormat :: String
+timeFormat = "%r"
+
+timeFormatTZ :: String
+timeFormatTZ = "%r %Z"
+
+dateTimeFormat :: String
+dateTimeFormat = "%e %b %Y %H:%M:%S"
+
+dateTimeFormatTZ :: String
+dateTimeFormatTZ = "%e %b %Y %H:%M:%S %Z"
+
+------------------------------------------------------------------------
+
+formatDay :: Day -> String
+formatDay day = formatTime defaultTimeLocale dateFormat t
+    where
+        t :: UTCTime
+        t = UTCTime day 0
+
+formatDayTZ :: Day -> TimeLocale -> String
+formatDayTZ day tz = formatTime tz dateFormat t
+    where
+        t :: UTCTime
+        t = UTCTime day 0
+
+------------------------------------------------------------------------
+
+formatUTCTime :: UTCTime -> String
+formatUTCTime tm = formatTime defaultTimeLocale timeFormat tm
+
+formatUTCTimeTZ :: UTCTime -> TimeLocale -> String
+formatUTCTimeTZ tm tz = formatTime tz timeFormat tm
+
+------------------------------------------------------------------------
