393883666e655e65423a37e682e929c1a5e346aa
Sajith Sasidharan <sajith@nonzen.in>
2016-04-19 16:57:31 -0400

    remove Show instances of BGUnit and WtUnit.

--- Betty/Model.hs
+++ Betty/Model.hs
@@ -8,30 +8,22 @@ import ClassyPrelude.Yesod
 
 -- Blood glucose units.
 data BGUnit = MgDL | Mmol
-            deriving (Read, Eq, Enum, Bounded, Generic)
+            deriving (Show, Read, Eq, Enum, Bounded, Generic)
 
 derivePersistField "BGUnit"
 
 instance FromJSON BGUnit
 instance ToJSON BGUnit
 
-instance Show BGUnit where
-    show MgDL = "mg/dL"
-    show Mmol = "mmol"
-
 ------------------------------------------------------------------------
 
 -- Weight units.
 data WtUnit = Kg | Lb
-            deriving (Read, Eq, Enum, Bounded, Generic)
+            deriving (Show, Read, Eq, Enum, Bounded, Generic)
 
 derivePersistField "WtUnit"
 
 instance FromJSON WtUnit
 instance ToJSON WtUnit
-
-instance Show WtUnit where
-    show Kg = "kilogram"
-    show Lb = "pound"
 
 ------------------------------------------------------------------------
