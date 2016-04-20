d2d6e100a715268764d5220796782932391b2635
Sajith Sasidharan <sajith@nonzen.in>
2016-04-20 18:16:18 -0400

    reorganize postProfileBGUnitR.

--- Handler/ProfileBGUnit.hs
+++ Handler/ProfileBGUnit.hs
@@ -3,22 +3,30 @@ module Handler.ProfileBGUnit where
 import Betty.Model
 import Import
 
+------------------------------------------------------------------------
+
+-- TODO: rewrite this; write unit tests.
+
 postProfileBGUnitR :: Handler Html
 postProfileBGUnitR = do
 
     uid <- requireAuthId
 
-    unit <- runInputPost $ ireq textField "bgunits"
+    bgunit <- runInputPost $ ireq textField "bgunit"
 
-    let bgunit = case unit of
-            "mgdl" -> Just MgDL
-            "mmol" -> Just Mmol
+    $(logDebug) ("uid: " <> tshow uid <> ", bgunit: " <> tshow bgunit)
+
+    let unit = case bgunit of
+            "MgdL" -> Just MgDL
+            "Mmol" -> Just Mmol
             _      -> Nothing
 
-    when (isNothing bgunit) $
-        $(logError) ("User " <> tshow uid <>
-                     " chose unknown bg unit selected in settings")
+    when (isNothing unit) $
+        $(logError) ("User " <> tshow uid <> " chose " <>
+                     tshow unit <> " from settings")
 
-    runDB $ updateWhere [UserProfileUid ==. uid] [UserProfileBgunits =. bgunit]
+    runDB $ updateWhere [UserProfileUid ==. uid] [UserProfileBgunits =. unit]
 
     redirect ProfileR
+
+------------------------------------------------------------------------
