012777a11ca55aa027556bf7627d617ca4b56931
Sajith Sasidharan <sajith@nonzen.in>
2016-04-20 11:54:45 -0400

    implement postProfileBGUnitR
    Handler for setting blood sugar unit.
    
    See gh-3.

--- Handler/ProfileBGUnit.hs
+++ Handler/ProfileBGUnit.hs
@@ -1,6 +1,24 @@
 module Handler.ProfileBGUnit where
 
+import Betty.Model
 import Import
 
 postProfileBGUnitR :: Handler Html
-postProfileBGUnitR = error "Not yet implemented: postProfileBGUnitR"
+postProfileBGUnitR = do
+
+    uid <- requireAuthId
+
+    unit <- runInputPost $ ireq textField "bgunits"
+
+    let bgunit = case unit of
+            "mgdl" -> Just MgDL
+            "mmol" -> Just Mmol
+            _      -> Nothing
+
+    when (isNothing bgunit) $
+        $(logError) ("User " <> tshow uid <>
+                     " chose unknown bg unit selected in settings")
+
+    runDB $ updateWhere [UserProfileUid ==. uid] [UserProfileBgunits =. bgunit]
+
+    redirect ProfileR
