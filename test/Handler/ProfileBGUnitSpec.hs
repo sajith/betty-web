0940fc24fc84ddf98fb90837ee97ccd4a436d3c1
Sajith Sasidharan <sajith@nonzen.in>
2016-04-21 17:06:41 -0400

    Add (rudimentary!) tests for /profile/bgunit

--- test/Handler/ProfileBGUnitSpec.hs
+++ test/Handler/ProfileBGUnitSpec.hs
@@ -1,10 +1,17 @@
 module Handler.ProfileBGUnitSpec (spec) where
 
 import TestImport
+import TestTools
 
 spec :: Spec
 spec = withApp $ do
 
-    describe "postProfileBGUnitR" $ do
-        error "Spec not implemented: postProfileBGUnitR"
+    describe "test /profile/bgunit" $ do
+        it "get /profile/bgunit without authorization" $ do
+            get ProfileBGUnitR
+            statusIs 405
 
+        it "post /profile/bgunit without authorization" $ do
+            needsLogin POST ProfileBGUnitR
+
+        -- TODO: other tests
