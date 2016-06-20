9aa08cd2082eece7331d1b4c8f53e9b7b25cfbd9
Sajith Sasidharan <sajith@nonzen.in>
2016-06-20 10:48:21 -0400

    `hlint`-ed.

--- test/Handler/ProfileBGUnitSpec.hs
+++ test/Handler/ProfileBGUnitSpec.hs
@@ -4,14 +4,14 @@ import TestImport
 import TestTools
 
 spec :: Spec
-spec = withApp $ do
+spec = withApp $
 
     describe "test /profile/bgunit" $ do
         it "get /profile/bgunit without authorization" $ do
             get ProfileBGUnitR
             statusIs 405
 
-        it "post /profile/bgunit without authorization" $ do
+        it "post /profile/bgunit without authorization" $
             needsLogin POST ProfileBGUnitR
 
         -- TODO: other tests
