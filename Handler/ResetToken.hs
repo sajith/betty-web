ca8a2d6b7695e6cf46f66e3bbf49076d29c1034f
Sajith Sasidharan <sajith@nonzen.in>
2016-06-29 14:47:10 -0400

    Simplify `postResetTokenR`

--- Handler/ResetToken.hs
+++ Handler/ResetToken.hs
@@ -4,23 +4,26 @@ import Import
 
 import Betty.Token (newToken, setToken)
 
+------------------------------------------------------------------------
+
 -- TODO: write tests
 
 postResetTokenR :: Handler Html
 postResetTokenR = do
 
     -- no representation without authorization
-    Entity k u <- requireAuth
-    let email = userEmail u
+    uid <- requireAuthId
 
     -- generate new api key
     token <- lift newToken
 
-    $(logDebug) ("postResetTokenR: email: " <> email
-                 <> ",token:" <> token <> "\n")
+    $logDebug ("postResetTokenR: uid: " <> tshow (unUserKey uid)
+               <> ", new token:" <> token)
 
     -- update database
-    _ <- setToken k token
+    _ <- setToken uid token
 
     -- return to /profile page.
     redirect ProfileR
+
+------------------------------------------------------------------------
